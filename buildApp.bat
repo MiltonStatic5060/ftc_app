8f3ef03fd7ba0a2af5cb9ed31be4cfb648c4be31
Robot Club <mhsrobotclub@gmail.com>
2018-05-30 20:32:56 -0400

    added new command options to build scripts

--- buildApp.bat
+++ buildApp.bat
@@ -1,2 +1,2 @@
-gradlew.bat assembleDebug && copy /Y TeamCode\build\outputs\apk\TeamCode-debug.apk apk\TeamCode-debug.apk && echo The newest apk is in the apk folder. && pause
+cmd /k gradlew.bat assembleDebug && copy /Y TeamCode\build\outputs\apk\TeamCode-debug.apk apk\TeamCode-debug.apk && echo The newest apk is in the apk folder. && pause
 pause
--- installApp.bat
+++ installApp.bat
@@ -1,2 +1,2 @@
-..\..\android-sdk\platform-tools\adb.exe devices && ..\..\android-sdk\platform-tools\adb.exe install -r apk\TeamCode-debug.apk && pause
+cmd /k ..\..\android-sdk\platform-tools\adb.exe devices && ..\..\android-sdk\platform-tools\adb.exe install -r apk\TeamCode-debug.apk && pause
 pause
